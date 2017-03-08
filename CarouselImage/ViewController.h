3c897152adb433c48c73ab14dde329efa472ac29
xubojoy <xubojoy1112@163.com>
2017-03-08 10:35:00 +0800

    Initial Commit

---
+++ CarouselImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarouselImage
+//
+//  Created by xubojoy on 2017/3/8.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CarouselImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CarouselImage
+//
+//  Created by xubojoy on 2017/3/8.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
